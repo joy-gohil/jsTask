d53d197703dc53bc816187ea370bf262869d3e76
Ritesh Tailor <ritesh.tailor@rapidops.com>
2019-02-12 11:54:51 +0530

    Add AssignCourse method in Admin Class

--- Admin.js
+++ Admin.js
@@ -29,4 +29,12 @@ class Admin{
         this.lmsMaster.participants = participant;
         return participant;
     }
+
+    assignCourse(participantId, courseId){
+        let participantIndex = _.findIndex(this.lmsMaster.participants,{ userId: participantId });
+        let courseIndex = _.findIndex(this.lmsMaster.courses,{ courseId: courseId });
+
+        let course = this.lmsMaster._courses[courseIndex];
+        this.lmsMaster._participants[participantIndex].coursesTaken = course;
+    }
 }
